b0c506cb7a4a74800acecd546790441f174f4720
James C Kimble Jr <me@jckimble.com>
2022-07-19 05:35:30 -0500

    Update release.yml

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
+import os, subprocess
 
 with open("README.md","r",encoding="utf-8") as fh:
     long_description=fh.read()
 
+if os.path.isdir(".git"):
+    ver=subprocess.check_output("git describe --tags --abbrev=0 | sed 's/^v//;'",shell=True).decode('utf-8').replace('\n','')
+    with open("version","w",encoding="utf-8") as fh:
+        fh.write(ver)
+elif os.path.isfile("version"):
+    with open("version","r",encoding="utf-8") as fh:
+        ver=fh.read().replace('\n','')
+else:
+    ver="0.0.0"
+
 setup(
     name='pacrepo-cli',
-    version="1.1.0",
+    version=ver,
     author='James C Kimble',
     author_email='me@jckimble.com',
     license='ISC',
